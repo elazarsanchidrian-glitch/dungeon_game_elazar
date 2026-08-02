a8c5dc769cb0df60228099fc5dc3063bdc9c646f
Elazar <elazarsanchidrian@gmail.com>
2026-08-02 11:46:09 +0300

    added more file folders created a class and defined the classes

--- elazar_monster_class.py
+++ elazar_monster_class.py
@@ -1,3 +1,15 @@
 class Monster:
-    def __init__(self):
+
+    def __init__(self,name):
+        self.name = name
+        self.health = 100
+
+
+    def attack(self):
+        pass
+
+    def take_damage(self):
+        self.health -= 10
+
+    def is_alive(self):
         pass
--- elazar_player_class.py
+++ elazar_player_class.py
@@ -1,3 +1,16 @@
 class Player:
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
+        self.health = 100
+        self.stamina = 100
+
+
+
+    def move(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def take_damage(self):
         pass
