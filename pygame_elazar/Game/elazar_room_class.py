80a1a69362ef99a63e209a7b0d2d38c521c2d88b
Elazar Sanchidrian <elazarsanchidrian@gmail.com>
2026-08-05 10:17:27 +0300

    added raw dungeon crawler game files

---
+++ pygame_elazar/Game/elazar_room_class.py
@@ -0,0 +1,17 @@
+class Level:
+
+    def __init__(self,name):
+        self.name = name
+
+
+    def remove_item(self,item):
+        pass
+
+
+    def add_monster(self,monster):
+        pass
+
+
+    def display(self):
+        pass
+
